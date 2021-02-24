635c17211c97d5a4b05947727ac23769557c15e4
wekonu <wahjoe.eko.n@gmail.com>
2021-02-24 11:11:49 +0700

    +- mvc4 Controller

--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,37 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
+        // controller
         $url = $this->parseURL();
-        var_dump($url);
+        if(file_exists('../app/controllers/' . $url[0] . '.php')){
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // method
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // params
+        if(!empty($url)){
+            $params = array_values($url);
+        }
+
+        // run controller and method,send params if any
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL(){
