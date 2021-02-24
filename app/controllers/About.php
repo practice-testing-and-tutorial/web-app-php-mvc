4ff980771882df0b3ae15988832f9b5f7b93712f
wekonu <wahjoe.eko.n@gmail.com>
2021-02-24 11:48:29 +0700

    +- mvc5 View, templates

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -2,10 +2,16 @@
 
 class About extends Controller {
     public function index(){
+        $data['title'] = 'About';
+        $this->view('templates/header', $data);
         $this->view('about/index');
+        $this->view('templates/footer');
     }
 
     public function page(){
+        $data['title'] = 'Page';
+        $this->view('templates/header', $data);
         $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
