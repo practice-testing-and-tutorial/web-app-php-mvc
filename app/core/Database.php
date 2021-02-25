161500f1a08ee22f2111b3f97b561a3c29d48800
wekonu <wahjoe.eko.n@gmail.com>
2021-02-25 15:03:37 +0700

    - mvc9 insert data to db, implementing bootstrap modal box

--- app/core/Database.php
+++ app/core/Database.php
@@ -63,4 +63,8 @@ class Database {
         $this->execute();
         return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function changedRowCount(){
+        return $this->stmt->rowCount(); // returns affected/changed row
+    }
 }
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -19,4 +19,18 @@ class Mahasiswa_model {
         $this->db->bind('id', $id);
         return $this->db->single();
     }
+
+    public function tambahDataMahasiswa($data){
+        $query = "INSERT INTO " . $this->table . " VALUES ('', :nama, :nrp, :email, :jurusan)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+
+        return $this->db->changedRowCount();
+    }
 }
